807a468cf5ec91ae0da005023fced79b08a0b5bd
mbabic <mbabic@ualberta.ca>
2014-03-15 10:20:19 -0600

    Config files added, run_tests.sh updated.

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,16 @@
 # Assumes *nix-like environment (originally implemented to be run cygwin)
 
 # Path to test .yuv files. 
-_yuv_path="/cygdrive/c/Users/Marko/YUV"
+_yuv_files="/cygdrive/c/Users/Marko/VideoCodecComp/*"
 
 # Path to HM HEVC encoder.
-_hm_hevc_encoder="/cygdrive/c/Users/Marko/HM_HEVC/bin"
+_hm_hevc_encoder="/cygdrive/c/Users/Marko/HM_HEVC/bin/vc10/Win32/Debug/TAppEncoder.exe"
 
 # Path to HM HEVC configuration files.
 _hm_hevc_cfg="./cfg/hm_hevc/"
+
+# Path to HM HEVC non-file-specific config file.
+_hm_hevc_global_cfg="./cfg/hm_hevc/encoder_randomaccess_main10.cfg"
 
 # Path to x.265 configuration files.
 _x265_cfg="./cfg/x265/"
@@ -33,9 +36,20 @@ hm_hevc_test() {
 	cfg_name=$(echo $filename | cut -d . -f 1)
 	cfg=$_hm_hevc_cfg$cfg_name.cfg
 
+	echo $1
+
+	exec_str="$_hm_hevc_encoder -i $1 -c $cfg -c $_hm_hevc_global_cfg"
+
+	#eval "$exec_str"
+
 } 
 
 # Iterate over all test files.
-for f in "$_yuv_path"/*.yuv; do
-	hm_hevc_test $f
+for f in $_yuv_files; 
+do
+	echo $f
+	if [[ "$f" =~ *\.yuv ]]; then
+		echo "matched"
+		hm_hevc_test $f
+	fi
 done
