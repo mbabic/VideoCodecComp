74ae2bad1b0d435409cc9a1af417e96ad5e937d0
mbabic <mbabic@ualberta.ca>
2014-03-15 12:49:33 -0600

    Tester updated.  Successfully ran test for HM HEVC codec on one file.

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,16 @@
 # Assumes *nix-like environment (originally implemented to be run cygwin)
 
 # Path to test .yuv files. 
-_yuv_files="/cygdrive/c/Users/Marko/VideoCodecComp/*"
+_yuv_files="./yuv/*"
 
 # Path to HM HEVC encoder.
 _hm_hevc_encoder="/cygdrive/c/Users/Marko/HM_HEVC/bin/vc10/Win32/Debug/TAppEncoder.exe"
 
 # Path to HM HEVC configuration files.
 _hm_hevc_cfg="./cfg/hm_hevc/"
+
+# Path to HM HEVC output folder.
+_hm_hevc_out="./out/hm_hevc"
 
 # Path to HM HEVC non-file-specific config file.
 _hm_hevc_global_cfg="./cfg/hm_hevc/encoder_randomaccess_main10.cfg"
@@ -33,23 +36,23 @@ hm_hevc_test() {
 	# By convention we have that the name of the config file will be the
 	# same as the .yuv video it is meant to test but with the appropriate
 	# extension (.cfg)
-	cfg_name=$(echo $filename | cut -d . -f 1)
-	cfg=$_hm_hevc_cfg$cfg_name.cfg
+	name=$(echo $filename | cut -d . -f 1)
+	cfg=$_hm_hevc_cfg$name.cfg
 
-	echo $1
+	# Specify the name and location of the output file to be generated.
+	out=$_hem_hevc_out$name.decoded
 
 	exec_str="$_hm_hevc_encoder -i $1 -c $cfg -c $_hm_hevc_global_cfg"
 
-	#eval "$exec_str"
+	echo $exec_str
+
+	eval "$exec_str"
 
 } 
 
 # Iterate over all test files.
 for f in $_yuv_files; 
 do
-	echo $f
-	if [[ "$f" =~ *\.yuv ]]; then
-		echo "matched"
-		hm_hevc_test $f
-	fi
+	# TODO: ensure that the extension of the file is .yuv
+	hm_hevc_test $f
 done
