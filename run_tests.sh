23032d1a37067432d572b84eb469f1586a79f132
Marko Babic <mbabic@ualberta.ca>
2014-04-13 18:29:56 +0100

    Finally committing results files.

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@
 _yuv_files="./yuv/*"
 
 # Path to HM HEVC encoder.
-_hm_hevc_encoder="/cygdrive/c/Users/Marko/HM_HEVC/bin/vc10/Win32/Debug/TAppEncoder.exe"
+_hm_hevc_encoder="encoders/hm_hevc/TAppEncoder.exe"
 
 # Path to HM HEVC configuration files.
 _hm_hevc_cfg="./cfg/hm_hevc/"
@@ -15,16 +15,26 @@ _hm_hevc_cfg="./cfg/hm_hevc/"
 # Path to HM HEVC output folder.
 _hm_hevc_out="./out/hm_hevc"
 
-_results_dir="./results"
-
 # Path to HM HEVC non-file-specific config file.
 _hm_hevc_global_cfg="./cfg/hm_hevc/encoder_randomaccess_main10.cfg"
 
-# Path to x.265 configuration files.
-_x265_cfg="./cfg/x265/"
+_results_dir="./results"
+
+
+# Path to x265 encoder
+_x265_encoder="encoders/x265/x265.exe"
+
+# Path to x265 output directory
+_x265_out="out/x265"
+
+# Path to x264 encoder
+_x264_encoder="encoders/x264/x264.exe"
+
+# Path to x264 output directory
+_x264_out="out/x264"
 
 # Target quantization parameter values.
-declare -a _qps=("50" "32" "27")
+declare -a _qps=("50" "32")
 
 # /Configuration variables. ####################################################
 
@@ -61,11 +71,11 @@ hm_hevc_test() {
 	name=$(echo $filename | cut -d . -f 1)
 	cfg=$_hm_hevc_cfg$name.cfg
 
-	# Iterate over all target bitrates.
+	# Iterate over all target quantization parameters.
 	for QP in "${_qps[@]}"
 	do
 
-		# Specify new bitrate in the configuration file.
+		# Specify new qp in the configuration file.
 		sed -i "s/\(QP[[:space:]]*:[[:space:]]*\).*/\1$QP/" $cfg		
 		# Specify the name and location of the output file to be generated.
 		bin_out=$_hm_hevc_out$name.bin
@@ -94,9 +104,82 @@ hm_hevc_test() {
 	done
 } 
 
+x265_test() {
+
+	results_file="$_results_dir/x265_results.txt"
+	record "------------ Testing x265 ------------" $results_file
+	record "" $results_file	
+
+        # Get file name.
+	tmp=$(echo $1 | awk -F\\ '{print $(NF)}')
+	filename=$(echo $tmp | awk -F/ '{print $(NF)}')
+    	name=$(echo $filename | cut -d . -f 1)
+
+        # Split string at underscores
+        IFS='_' read -ra PARAMS <<< "$name"
+        
+	# Iterate over all target quantization parameters.
+	for QP in "${_qps[@]}"
+	do
+                bin_out="$_x265_out""/""$filename"".hevc"
+                rec_out="$_x265_out""/""$filename"".yuv"
+		exec_str="$_x265_encoder --input $1 --output $bin_out -f 100 --input-res ${PARAMS[1]} --fps ${PARAMS[2]} --qp $QP"
+
+		# Start timer.
+		#START=$(data +%s.%N)
+
+		eval "$exec_str" >> $results_file
+
+		# End timer.
+		#END=$(date +%s.%N)
+
+		#RUN_TIME=$(echo "$END - $START" | bc)
+		record $RUN_TIME $results_file
+
+		record "---------------------------------------------------------------" $results_file
+	done
+
+}
+
+x264_test() {
+	results_file="$_results_dir/x264_results.txt"
+	record "------------ Testing x264 ------------" $results_file
+	record "" $results_file	
+
+        # Get file name.
+	tmp=$(echo $1 | awk -F\\ '{print $(NF)}')
+	filename=$(echo $tmp | awk -F/ '{print $(NF)}')
+    	name=$(echo $filename | cut -d . -f 1)
+
+        # Split string at underscores
+        IFS='_' read -ra PARAMS <<< "$name"
+ 	# Iterate over all target quantization parameters.
+	for QP in "${_qps[@]}"
+	do
+                bin_out="$_x264_out""/""$filename"".mp4"
+                rec_out="$_x264_out""/""$filename"".yuv"
+		exec_str="$_x264_encoder --output NUL --frames 100 --input-res ${PARAMS[1]} --fps ${PARAMS[2]} --qp $QP $1"
+
+		# Start timer.
+		#START=$(data +%s.%N)
+
+		eval "$exec_str" >> $results_file
+
+		# End timer.
+		#END=$(date +%s.%N)
+
+		#RUN_TIME=$(echo "$END - $START" | bc)
+		record $RUN_TIME $results_file
+
+		record "---------------------------------------------------------------" $results_file
+	done
+
+}
+
 # Iterate over all test files.
 for f in $_yuv_files; 
 do
-	# TODO: ensure that the extension of the file is .yuv
-	hm_hevc_test $f
+	#hm_hevc_test $f
+        #x265_test $f
+        x264_test $f
 done
